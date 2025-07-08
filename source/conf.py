f5052c9a5f4a1352a4bc7748bc80c40e33c7e2c7
zhaojiedi <zhaojiedi@kuaishou.com>
2025-07-08 09:49:35 +0800

    add

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,7 @@ release = 'v1.0.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_comments', 
+    'sphinx_comments_zhaojiedi', 
     # ... 其他扩展
 ]
 
